60ce811b84034626995fa7c973d3729208f9e1d6
luis24552-cmd <luis24552@cbtis75.edu.mx>
2025-09-29 18:09:52 -0600

    Update heptagono.py

--- proyecto_figuras/interfaces/heptagono.py
+++ proyecto_figuras/interfaces/heptagono.py
@@ -1,14 +1,14 @@
 from interfaces.figura import Figura
-import math
 
-class Hexagono(Figura):
+class Heptagono(Figura):
     def __init__(self, lado: float, apotema: float):
         self.lado = lado
         self.apotema = apotema
 
     def area(self) -> float:
-        perimetro = 6 * self.lado
+        perimetro = 7 * self.lado
         return (perimetro * self.apotema) / 2
 
     def perimetro(self) -> float:
-        return 6 * self.lado
+        return 7 * self.lado
+
